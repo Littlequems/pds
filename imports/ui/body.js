8adf79a4832c40c1cb4fe399006071891b520618
Littlequems <q.alefe_98@hotmail.com>
2019-10-22 16:29:23 -0400

    formulario2

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,24 +6,7 @@ import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
-  },
-});
-Template.body.events({
-  'submit .new-task'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
-    // Get value from form element
-    const target = event.target;
-
-    const text = target.text.value;
-
-    // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
-    // Clear form
-    target.text.value = '';
-  },
+    // Show newest tasks at the top
+  return Tasks.find({}, { sort: { createdAt: -1 } });
+},
 });
